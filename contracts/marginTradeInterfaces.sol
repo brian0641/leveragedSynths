32df5681ba91ba4be25f7d29d4a7ddcd1468d299
brian0641 <setlinger@protonmail.com>
2020-03-07 15:36:49 -0500

    initial commit

---
+++ contracts/marginTradeInterfaces.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.11;
+
+contract SynthetixInterface {
+    function exchange(bytes32 sourceCurrencyKey, uint sourceAmount, bytes32 destinationCurrencyKey)
+        external 
+        returns (uint amountReceived);
+    function synths(bytes32 currencyKey) public view returns (address);
+}
+
+contract SynthInterface {
+    function currencyKey() public view returns (bytes32 _currencyKey);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function balanceOf(address _owner) public view returns (uint256 balance);
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool);
+}
+
+contract ExchRatesInterface {
+    function rateForCurrency(bytes32 currencyKey) external view returns (uint);
+    function ratesForCurrencies(bytes32[] calldata currencyKeys) external view returns (uint256[] memory);
+}
+
+
+
